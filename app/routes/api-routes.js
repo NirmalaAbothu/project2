8a8f97c8a7c74a762ae4534f061b17b0dd1c5560
BrandonVA <codebva@gmail.com>
2020-12-14 18:50:11 -0800

    Addded routes and logic to create recipients and a route to view all recipients for a user.

--- app/routes/api-routes.js
+++ app/routes/api-routes.js
@@ -1,9 +1,26 @@
 const db = require('../models/index')
 
+
 module.exports = app => {
     app.post('/api/createUser', (req, res) => {
         db.User.create(req.body).then(response => {
             res.send(response)
         })
     })
+    app.post('/api/newRecipient', (req, res) => {
+        db.Recipients.create(req.body).then(response => {
+            res.send(response)
+        })
+    })
+    app.get('/api/allRecipients/:id_user', (req, res) =>{
+        db.Recipients.findAll({
+            where: {
+                id_user: req.params.id_user
+            },
+            include: [db.User]
+          }).then(function(response) {
+            res.json(response);
+          });
+    })
+    
 }

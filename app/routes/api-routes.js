ddefa2d9d110bd0cd9606f890038aaba96f82cc0
BrandonVA <codebva@gmail.com>
2020-12-17 21:59:32 -0800

    Cleaned up code.

--- app/routes/api-routes.js
+++ app/routes/api-routes.js
@@ -5,7 +5,7 @@ module.exports = app => {
 
     app.post('/api/login', passport.authenticate("local"), (req, res) => {
         res.json(req.user);
-        console.log(req.user);
+        // console.log(req.user);
     })
 
     app.post('/api/signUp', (req, res) => {
@@ -20,21 +20,11 @@ module.exports = app => {
                 res.status(401).json(err);
             });
     })
-    // NEED TO PUT AFTER SETTING ID IN LOCAL STORAGE
-    //window.location.replace("/members");
-
-    // app.post("/api/signup", function(req, res) {
-    //     db.User.create({
-    //       email: req.body.email,
-    //       password: req.body.password
-    //     })
-    //       .then(function() {
-    //         res.redirect(307, "/api/login");
-    //       })
-    //       .catch(function(err) {
-    //         res.status(401).json(err);
-    //       });
-    //   });
+  // Route for logging the user out
+  app.get("/logout", (req, res) => {
+    req.logout();
+    res.redirect("/");
+  });
 
     app.post('/api/newRecipient', (req, res) => {
         db.Recipients.create(req.body).then(response => {
@@ -48,33 +38,12 @@ module.exports = app => {
         db.Recipients.findAll({
             where: {
                 id_user: req.user.id,
-            },
+            }
             // include: [db.User]
-        }).then(function (recipients) {
-            // res.json(recipients);
+        }).then( (recipients) => {
 
-            // Declaring an array to hold all the recipient id and name values as an object for each recipient 
-            const parsedData = []
-            //  If the db response is greater than 0 than...
-            if (recipients.length > 0) {
-                // Loop through the data and push an object containing the recipients id and name values.
-                recipients.forEach(element => {
-                    const data = { id: element.id, name: element.name }
-                    parsedData.push(data)
-                });
-            }
-            // Creating an object to hold the pared data to send out via handlebars.
-            const recipientsData = {
-                recipients: parsedData
-            }
-            res.json(recipientsData)
-        // db.Recipients.findAll({
-        //     where: {
-        //         id_user: req.params.id_user
-        //     },
-        //     include: [db.User]
-        // }).then(function (response) {
-        //     res.json(response);
+            res.json(recipients)
+
         });
     });
 

165f9374ceb694d99c942826812f7f985253ccda
BrandonVA <codebva@gmail.com>
2020-12-18 10:13:00 -0800

    Added Snow animation and christmas countdown.

--- app/public/js/login.js
+++ app/public/js/login.js
@@ -5,22 +5,22 @@ $(document).ready(() => {
 
      loginButton.on("click", (event) => {
           event.preventDefault();
-          
+
           const userInfo = {
                email: emailInput.val().trim(),
                password: passwordInput.val().trim()
           };
-          
+
           console.log(emailInput.val());
 
           console.log(userInfo);
-            
+
           if (!userInfo.email || !userInfo.password) {
-               return; 
+               return;
           };
 
           userLogin(userInfo.email, userInfo.password);
-             
+
           emailInput.val("");
           passwordInput.val("");
      })
@@ -35,8 +35,57 @@ $(document).ready(() => {
                console.log(err);
           });
      };
-});
 
+     // 
+     // Snow animation and christmas countdown credit goes to the awesome person below!!!
+     //_______________________________https://codepen.io/sashatran/pen/mOWLLB  ______________________________________ 
+
+     const myDate = new Date();
+     const xmas = Date.parse("Dec 25, " + myDate.getFullYear())
+     const today = Date.parse(myDate)
+
+     const daysToChristmas = Math.round((xmas - today) / (1000 * 60 * 60 * 24))
+
+
+     if (daysToChristmas == 0)
+          $('#days').text("It's Christmas!! Merry Christmas!");
+
+     if (daysToChristmas < 0)
+          $('#days').text("Christmas was " + -1 * (daysToChristmas) + " days ago.");
+
+     if (daysToChristmas > 0)
+          $('#days').text(daysToChristmas + " days to Christmas!");
+
+     //make snow
+     snowDrop(150, randomInt(1035, 1280));
+     snow(150, 150);
+
+     function snow(num, speed) {
+          if (num > 0) {
+               setTimeout(function () {
+                    $('#drop_' + randomInt(1, 250)).addClass('animate');
+                    num--;
+                    snow(num, speed);
+               }, speed);
+          }
+     };
+
+     function snowDrop(num, position) {
+          if (num > 0) {
+               var drop = '<div class="drop snow" id="drop_' + num + '"></div>';
+
+               $('body').append(drop);
+               $('#drop_' + num).css('left', position);
+               num--;
+               snowDrop(num, randomInt(60, 1280));
+          }
+     };
+
+     function randomInt(min, max) {
+          return Math.floor(Math.random() * (max - min + 1) + min);
+     };
+     // __________________________________________________________________________________________________________
+});
 
 // $(document).ready(function () {
 //      // Getting jquery references

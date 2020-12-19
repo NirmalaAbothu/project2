0de3b84b9adf4dc3cae33c4d932ff257dda47ee4
Cynthia Wu <cynthia21wu@gmail.com>
2020-12-19 12:27:15 -0800

    Adding Readme again

--- app/config/config.js
+++ app/config/config.js
@@ -4,7 +4,7 @@ module.exports = {
     "development": {
       "username": "root",
       // "password": process.env.DBPASSWORD,
-      "password": 'Rootkitten921!',
+      "password": 'password',
       "database": "xmasList_db",
       "port": 3306,
       "host": "127.0.0.1",

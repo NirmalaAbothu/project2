f68e7fbdf6e75f9af31379b54c38b100e6764479
Nirmala <nirmala_ira@hotmail.com>
2020-12-19 10:40:06 -0800

    added members.html

--- app/public/js/members.js
+++ app/public/js/members.js
@@ -1,4 +1,39 @@
 // Make sure we wait to attach our handlers until the DOM is fully loaded.
+
+$(document).ready(function () {
+     $(".sidenav").sidenav();
+     $("#sidenav-1").sidenav({ edge: "left" });
+
+     $.get("/api/allRecipients").then((res) => {
+          var result = recipients;
+          // The <ul> that we will add <li> elements to:
+          let myList = document.querySelector("ul.mylist");
+
+          // Create an <li> element:
+          for (let i = 0; i < result.Length; i++) {
+               let li = document.createElement("li");
+
+               // Give it the desired classes & attributes:
+               li.classList.add("ui-menu-item");
+               li.setAttribute("name", "menuitem");
+
+               // Now create an <a> element:
+               let a = document.createElement("a");
+
+               // Give it the desired classes & attributes:
+               a.classList.add("ui-all");
+
+               a.innerText = result[i].name; //  <--- the recipient name from the result
+               a.href = app.get("/gifts/:result[i].name");
+               // li.setAttribute()
+
+               // Now add the <a> to the <li>, and add the <li> to the <ul>
+               li.appendChild(a);
+               myList.appendChild(li);
+          }
+     });
+});
+
 $(function () {
      // adding new recipient
 

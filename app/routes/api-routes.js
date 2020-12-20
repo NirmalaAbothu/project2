2678ccc285771c59b60ad10cde827de529c0e5aa
BrandonVA <codebva@gmail.com>
2020-12-20 15:13:10 -0800

    cleaned up code and added comments.

--- app/routes/api-routes.js
+++ app/routes/api-routes.js
@@ -3,28 +3,36 @@ const passport = require("../config/passport");
 
 module.exports = app => {
 
+    // Login the user and send back user data or an error if it fails.
     app.post('/api/login', passport.authenticate("local"), (req, res) => {
         res.json(req.user);
-        
     })
 
+    // signUp route will... 
     app.post('/api/signUp', (req, res) => {
-        db.User.create(req.body).then(response => {
-
-        }).then(function () {
-            res.redirect(307, "/api/login");
-        })
+        // create a new user 
+        db.User.create(req.body)
+            .then(() => {
+                // then redirect tot api/login to authenticate the user.
+                res.redirect(307, "/api/login");
+            })
             .catch(function (err) {
+                // or send back the err
                 res.status(401).json(err);
             });
     })
-    // Route for logging the user out
+
+    // Route for logging the user...  
     app.get("/logout", (req, res) => {
+        // log them out then send them back to the home page.
         req.logout();
         res.redirect("/");
     });
 
+    // Route for creating a new recipient expects a string for a name.
     app.post('/api/newRecipient/:name', (req, res) => {
+        // Creating the new recipient in the db
+        // Setting the name value with the passed param and user id with the current users id.
         db.Recipients.create(
             {
                 name: req.params.name,
@@ -35,37 +43,43 @@ module.exports = app => {
         })
     })
 
+    // Route for getting all the recipients for the current user
     app.get('/api/allRecipients', (req, res) => {
+        // Finding all recipients for the logged in user
         db.Recipients.findAll({
             where: {
                 id_user: req.user.id,
             }
         }).then((recipients) => {
-
+            // Sending the recipients back as an array.
             res.json(recipients)
-
         });
     });
 
-
+    // Route for posting a new gift.
     app.post('/api/newGift', (req, res) => {
+        // Expects a string in the response to name the gift as well as an id for the recipient it belongs to.
         db.Gifts.create(req.body).then(response => {
             res.json(response);
         })
     });
 
-
+    // Route for getting all gifts for a specific recipient.
     app.get('/api/allGifts/:id_recipient', (req, res) => {
+        // expecting a num for the id of the recipient to be passed as a param.
         db.Gifts.findAll({
             where: {
                 id_recipient: req.params.id_recipient
             }
         }).then(function (response) {
+            // sending back all the gifts for that recipient as an array.
             res.json(response);
         });
     });
 
+    // Route for deleting a recipient based on their id.
     app.get('/api/deleteRecipient/:id_recipient', (req, res) => {
+        // Search and destroy...
         db.Recipients.destroy({
             where: {
                 id: req.params.id_recipient
@@ -75,7 +89,9 @@ module.exports = app => {
         });
     });
 
-    app.get('/api/deleteGift/:id_gift/:id_recipient', (req, res) => {
+    // Route for deleting a gift based on the gift id.
+    app.get('/api/deleteGift/:id_gift', (req, res) => {
+        // Search and destroy...
         db.Recipients.destroy({
             where: {
                 id: req.params.id_gift
@@ -83,6 +99,5 @@ module.exports = app => {
         }).then(response => {
             res.end(response);
         });
-
     });
 }

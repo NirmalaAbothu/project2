4f8127ccb6dff686922a40815ca98d2258405969
Alischrec <alischreck@gmail.com>
2020-12-22 14:35:21 -0800

    pushing to pull from develop

--- app/config/config.js
+++ app/config/config.js
@@ -3,8 +3,8 @@ require("dotenv").config();
 module.exports = {
     "development": {
       "username": "root",
-      "password": process.env.DBPASSWORD,
-      // "password": 'password',
+      // "password": process.env.DBPASSWORD,
+      "password": 'password',
       "database": "xmasList_db",
       "port": 3306,
       "host": "127.0.0.1",
@@ -13,7 +13,7 @@ module.exports = {
     "test": {
       "username": "root",
       // "password": process.env.DBPASSWORD,
-      // "password": 'password',
+      "password": 'password',
       "database": "xmasList_db",
       "host": "127.0.0.1",
       "dialect": "mysql"
